f4320d4a97739570f75ab82afb0afdadb61b0c8f
Sakhawat Rifat <sakhawatrifat99@gmail.com>
2021-09-08 10:47:41 +0600

    Everyting Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,35 +40,35 @@
       });
     });
 	// Counter 
-	$(document).ready(function(){
-		$(window).scroll(testScroll);
-		var viewed = false;
-		function isScrolledIntoView(elem) {
-		    var docViewTop = $(window).scrollTop();
-		    var docViewBottom = docViewTop + $(window).height();
+	// $(document).ready(function(){
+	// 	$(window).scroll(testScroll);
+	// 	var viewed = false;
+	// 	function isScrolledIntoView(elem) {
+	// 	    var docViewTop = $(window).scrollTop();
+	// 	    var docViewBottom = docViewTop + $(window).height();
 
-		    var elemTop = $(elem).offset().top;
-		    var elemBottom = elemTop + $(elem).height();
+	// 	    var elemTop = $(elem).offset().top;
+	// 	    var elemBottom = elemTop + $(elem).height();
 
-		    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-		}
-		function testScroll() {
-		  if (isScrolledIntoView($(".counter-item-outer")) && !viewed) {
-		      viewed = true;
-		      $('.counter-number').each(function () {
-		      $(this).prop('Counter',0).animate({
-		          Counter: $(this).text()
-		      }, {
-		          duration: 3000,
-		          easing: 'swing',
-		          step: function (now) {
-		              $(this).text(Math.ceil(now));
-		          }
-		      });
-		    });
-		  }
-		}
-	});
+	// 	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	// 	}
+	// 	function testScroll() {
+	// 	  if (isScrolledIntoView($(".counter-item-outer")) && !viewed) {
+	// 	      viewed = true;
+	// 	      $('.counter-number').each(function () {
+	// 	      $(this).prop('Counter',0).animate({
+	// 	          Counter: $(this).text()
+	// 	      }, {
+	// 	          duration: 3000,
+	// 	          easing: 'swing',
+	// 	          step: function (now) {
+	// 	              $(this).text(Math.ceil(now));
+	// 	          }
+	// 	      });
+	// 	    });
+	// 	  }
+	// 	}
+	// });
 
 
 
